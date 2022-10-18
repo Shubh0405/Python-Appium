fc6eb28dac56d867437f111599ff4cb738bea0d9
Shubh Gupta <shubhngupta04@gmail.com>
2022-10-19 01:21:06 +0530

    Update test_allcategories.py

--- StepDefs/test_allcategories.py
+++ StepDefs/test_allcategories.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 scenarios("../features/CheckAllCategories.feature")
+scenarios("../features/SearchBox.feature")
 
 @given(u'Flipkart app opens')
 def LaunchFlipkartApp(context):
